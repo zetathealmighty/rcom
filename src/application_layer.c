88ac18856a47f7dd3b198aa78b152fb3f0e0c327
Tiago Gouveia <44753506+zetathealmighty@users.noreply.github.com>
2023-10-19 17:40:38 +0100

    Update application_layer.c

--- src/application_layer.c
+++ src/application_layer.c
@@ -20,32 +20,64 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         exit(1);
     }
 
-    if((int idfk = open(filename, O_RDONLY)) < 0) {
-        printf("fopen error");
-        exit(1);
-    }
-
     int bufSize = MAX_PAYLOAD_SIZE - 1;
     unsigned char buf[bufSize + 1];
 
-    int i = 1;
-
-    while(i > 0) {
-        i = read(idfk, buf + 1, bufSize);
-        if(i < 0) {
-            printf("fread error");
-            break;
-        } else if(i > 0) {
-            buf[0] = 1;
-            if(llwrite(buf, i) < 0) {
-                printf("llwrite error");
-                exit(1)
-            }
-        }
-    //not working, not done, finish later
-        
+    
+    switch(ll.role) {
+    	case LlTx: {
+		    if((FILE* file = fopen(filename, "rb")) == NULL) {
+        		printf("fopen error");
+        		exit(-1);
+    		}
+    		
+    		fseek(file, 0L, SEEK_END);
+    		
+    		long int fsize = ftell(file);
+    		
+    		rewind(file);
+    		
+			unsigned char* fcontent = (unsigned char*) malloc(fileLength);
+			fread(content, fileLength, file);
+			
+			long int i = fileSize;
+			
+			while(i > 0) {
+				int dataLen;
+				if(i > MAX_PAYLOAD_SIZE) {
+					dataLen = MAX_PAYLOAD_SIZE;
+				} else {
+					dataLen = i;
+				}
+				
+				unsigned char* data = (unsigned char*) malloc(datalen);
+				
+				memcpy(data, fcontent, dataLen);
+				
+				
+			}
+    		
+    		break;
+    	} case LlRx: {
+    	
+    		break;
+    	} default: {
+    		break;
+    	}
     }
-    
-    
-    
+   
+}
+
+unsigned char* makePacket(unsigned int which, unsigned char* data, int dataSize) {
+	int packetSize = datasize + 4;
+	
+	unsigned char* packet = (unsigned char*) malloc(packetSize);
+	
+	packet[0] = which;
+	packet[1] = dataSize >> 8 & 0xFF;
+	packet[2] = dataSize & 0xFF;
+	
+	memcpy(packet + 4, data, dataSize);
+	
+	return packet;
 }
